8bb16c8fc31e33cbd99023528ea679f2e3ab7c2e
sangyeol-kim <twysg@likelion.org>
2019-06-08 11:00:55 +0900

    Add express + vue environment

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const history = require('connect-history-api-fallback');
+
+const app = express();
+
+const port = 3000;
+
+const indexRouter = require('./routes/index');
+
+app.use(history());
+app.use(express.static('public'));
+
+app.use('/', indexRouter);
+
+app.listen(port, () => {
+    console.log(`서버가 ${port}에서 동작중입니다.`);
+});
